7ba28385cd854fe8e0f7d1c472d4dd2c51a60322
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2021-08-30 16:19:12 -0700

    h4 and content elements selected and assigned values.

--- js/index.js
+++ js/index.js
@@ -66,3 +66,21 @@ ctaImg.setAttribute('src', siteContent['cta']['img-src']);
 
 //footer
 
+const h4 = document.querySelectorAll('h4');
+console.log(h4);
+h4[0].textContent = siteContent['main-content']['features-h4'];
+h4[1].textContent = siteContent['main-content']['about-h4'];
+h4[2].textContent = siteContent['main-content']['services-h4'];
+h4[3].textContent = siteContent['main-content']['product-h4'];
+h4[4].textContent = siteContent['main-content']['vision-h4'];
+h4[5].textContent = siteContent['contact']['contact-h4'];
+
+
+const p = document.querySelectorAll('p');
+p[0].textContent = siteContent['main-content']['features-content'];
+p[1].textContent = siteContent['main-content']['about-content'];
+p[2].textContent = siteContent['main-content']['services-content'];
+p[3].textContent = siteContent['main-content']['product-content'];
+p[3].textContent = siteContent['main-content']['vision-content'];
+
+
